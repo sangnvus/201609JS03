49b3a40fc25c53998e565249c16a5df413096988
Đàm Huy Hùng <hungdhse03431@fpt.edu.vn>
2016-09-30 02:11:53 +0700

    1. refactor code for navigation drawer 2. add file xml in drawable

--- WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/AboutFragment.java
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/AboutFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class InformationFragment extends Fragment {
+public class AboutFragment extends Fragment {
 
 
-    public InformationFragment() {
+    public AboutFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class InformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
 }
--- WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/MainActivity.java
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/MainActivity.java
@@ -1,10 +1,9 @@
 package com.favn.firstaid;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.favn.firstaid.Activites.DirectionActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -29,14 +30,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -62,19 +63,30 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+        if (id == R.id.action_sos_calling) {
+            // TODO: Add calling action to 115
+        }
+        if (id == R.id.action_direction) {
+            startActivity(new Intent(this, DirectionActivity.class));
+        }
+        if(id == R.id.nav_history) {
+            // TODO: check suitable method
+            //android.support.v4.app.NavUtils.navigateUpFromSameTask(this);
+            //onNavigateUpFromChild(this);
+            onNavigateUp();
         }
 
         return super.onOptionsItemSelected(item);
@@ -85,26 +97,37 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
         FragmentManager fragmentManager = getSupportFragmentManager();
+        Class fragmentClass = null;
         if (id == R.id.nav_emergency) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new EmergencyFragment()).commit();
+            fragmentClass =  EmergencyFragment.class;
         } else if (id == R.id.nav_learning) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new LearningFragment()).commit();
+            fragmentClass = LearningFragment.class;
         } else if (id == R.id.nav_testing) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new TestFragment()).commit();
+            fragmentClass = TestFragment.class;
         } else if (id == R.id.nav_qa) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new QAFragment()).commit();
+            fragmentClass = QAFragment.class;
         } else if (id == R.id.nav_noti) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new NotificationFragment()).commit();
+            fragmentClass = NotificationFragment.class;
         } else if (id == R.id.nav_setting) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new SettingFragment()).commit();
+            fragmentClass =SettingFragment.class;
         } else if (id == R.id.nav_history) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new HistoryFragment()).commit();
+            fragmentClass =HistoryFragment.class;
         } else if (id == R.id.nav_infor) {
-            fragmentManager.beginTransaction().replace(R.id.fragment_container, new InformationFragment()).commit();
+            fragmentClass =AboutFragment.class;
         }
 
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setTitle(item.getTitle());
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
