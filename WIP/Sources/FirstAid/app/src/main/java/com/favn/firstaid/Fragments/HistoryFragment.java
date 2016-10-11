fc2484ea7ad4278e483f973b85cb09fb9ae0a1e3
dungnt3272 <vitieubu@gmail.com>
2016-10-11 23:27:32 +0700

    - action in history screen - show list Injury trong Learning và Testing screen - code giao diện Hỏi đáp

--- WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Fragments/HistoryFragment.java
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Fragments/HistoryFragment.java
@@ -4,8 +4,12 @@ package com.favn.firstaid.Fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.favn.firstaid.R;
 
@@ -26,7 +30,19 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         container.removeAllViews();
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_history, container, false);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.history, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(getActivity(), "Clear history", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
 }
--- WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Fragments/TestFragment.java
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Fragments/TestFragment.java
@@ -1,20 +1,32 @@
 package com.favn.firstaid.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.favn.firstaid.Activites.InstructionDetail;
+import com.favn.firstaid.Adapter.InjuryAdapter;
+import com.favn.firstaid.Database.DatabaseOpenHelper;
+import com.favn.firstaid.Models.Injury;
 import com.favn.firstaid.R;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TestFragment extends Fragment {
-
+public class TestFragment extends Fragment implements AdapterView.OnItemClickListener {
+    private InjuryAdapter adapter;
+    private DatabaseOpenHelper dbHelper;
+    private ListView listView;
+    private List<Injury> mInjuryList;
 
     public TestFragment() {
         // Required empty public constructor
@@ -24,9 +36,30 @@ public class TestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_test, container, false);
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        dbHelper = new DatabaseOpenHelper(getActivity());
+        dbHelper.createDatabase();
+        dbHelper.openDatabase();
+
+        mInjuryList = dbHelper.getListInjury();
+        adapter = new InjuryAdapter(getActivity(), mInjuryList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+        setHasOptionsMenu(true);
         container.removeAllViews();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return rootView;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Injury injury = (Injury) listView.getItemAtPosition(position);
+        int injuryId = injury.getId();
+
+        Intent intent = new Intent(getActivity(), InstructionDetail.class);
+        intent.putExtra("id", injuryId);
+        startActivity(intent);
+    }
 }
