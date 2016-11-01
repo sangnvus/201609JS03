5aefcf036f42499aaba0bf9f223ca44033737010
dungnt3272 <vitieubu@gmail.com>
2016-11-01 17:21:38 +0700

    Hung gia: audio play list instruction theo kind

---
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Activites/GetPhoneNoActivity.java
@@ -0,0 +1,20 @@
+package com.favn.firstaid.Activites;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import com.favn.firstaid.R;
+
+public class GetPhoneNoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_sending);
+
+    }
+
+}
--- WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Adapter/InstructionAdapter.java
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Adapter/InstructionAdapter.java
@@ -1,15 +1,19 @@
 package com.favn.firstaid.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.favn.firstaid.Activites.InstructionDetail;
 import com.favn.firstaid.Models.Instruction;
 import com.favn.firstaid.R;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -19,10 +23,12 @@ import java.util.List;
 public class InstructionAdapter extends BaseAdapter {
     private Context mContext;
     private List<Instruction> mInstructionList;
+    private boolean isEmegency;
 
-    public InstructionAdapter(Context mContext, List<Instruction> mInjuryList) {
+    public InstructionAdapter(Context mContext, List<Instruction> mInjuryList, boolean isEmegency) {
         this.mContext = mContext;
         this.mInstructionList = mInjuryList;
+        this.isEmegency = isEmegency;
     }
 
     @Override
@@ -45,16 +51,26 @@ public class InstructionAdapter extends BaseAdapter {
         View v = View.inflate(mContext, R.layout.item_instruction, null);
         TextView tvStep = (TextView) v.findViewById(R.id.text_step_number);
         TextView tvInstruction = (TextView) v.findViewById(R.id.text_instruction_content);
+
         ImageView imgImage = (ImageView) v.findViewById(R.id.image_instruction);
+
         //TODO check null for image property
         int imagePath = v.getResources().getIdentifier("com.favn.firstaid:drawable/" +
                 mInstructionList.get(position).getImage(), null, null);
 
+//        InputStream is = getClass().getResourceAsStream("com.favn.firstaid:drawable/" +
+//                mInstructionList.get(position).getAudio());
+
         tvStep.setText(mInstructionList.get(position).getStep() + "");
         tvInstruction.setText(mInstructionList.get(position).getInstruction());
+        if(!isEmegency){
+            TextView tvExplanation = (TextView) v.findViewById(R.id.text_instruction_explaination);
+            tvExplanation.setText("chưa có nội dung");
+        }
 
         imgImage.setImageResource(imagePath);
 
+//        Intent intent = new Intent(InstructionAdapter.this, InstructionDetail.class);
 
         return v;
     }
