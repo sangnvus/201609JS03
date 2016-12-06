50703882ae4555e0fed3b34fcc9940b7562275f3
dungnt3272 <vitieubu@gmail.com>
2016-12-06 16:44:14 +0700

    cardview banner

--- WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Activites/BannerDetail.java
+++ WIP/Sources/FirstAid/app/src/main/java/com/favn/firstaid/Activites/BannerDetail.java
@@ -1,8 +1,12 @@
 package com.favn.firstaid.activites;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.favn.firstaid.R;
 
@@ -12,6 +16,16 @@ public class BannerDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banner_detail);
+
+        CardView cv = (CardView) findViewById(R.id.cardView_115);
+        cv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.capcuu115hanoi.com/News.aspx?k=11&cate=74");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
